69df27eef5516225db3555a630a866bf251a6a4f
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2020-10-07 17:35:07 +0900

    commit

--- terminal.bat
+++ terminal.bat
@@ -1,3 +1,17 @@
 @echo off
+
+set CURRENT=%~dp0
+set BATPATH=%~f0
+IF %CURRENT:~0,3%==C:\ (
+  copy %BATPATH% Z:\terminal.bat > NUL
+  echo Z:\terminal.bat にコピーしました
+  echo 次回からは Z:\terminal.bat を開いてください
+  explorer /select,"Z:\terminal.bat" 
+  pause
+  goto END
+)
+
 set PATH=C:\Program Files\Java\jdk-14.0.2\bin\;%PATH%
 cmd
+
+:END
